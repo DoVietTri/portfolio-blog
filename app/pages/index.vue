d0b9a9ad190dd4911b0571136b88f147a1f55da5
Sotatek-Taylor <taylor.do@sotatek.com>
2025-09-12 13:41:17 +0700

    test seo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,6 +7,19 @@
   </UContainer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  title: "Taylor Do - Frontend Developer",
+  description: `
+    Welcome to my portfolio! I'm Taylor Do, a frontend developer based in Vietnam. I specialize in creating user-centered digital experiences that are both beautiful and functional
+  `,
+  ogTitle: "Taylor Do - Frontend Developer",
+  ogDescription: `
+    Welcome to my portfolio! I'm Taylor Do, a frontend developer based in Vietnam. I specialize in creating user-centered digital experiences that are both beautiful and functional
+  `,
+  ogImage: "/images/family.png",
+  twitterCard: "summary_large_image",
+});
+</script>
 
 <style scoped></style>
